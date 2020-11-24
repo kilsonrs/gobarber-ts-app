1b047bcf5b1316c8c64228e9d62e1b6a9eb54688
Kilson <kilsonrs@gmail.com>
2020-11-24 09:38:02 -0300

    Criando a estrutura da tela

--- src/pages/AppointmentCreated/index.tsx
+++ src/pages/AppointmentCreated/index.tsx
@@ -1,9 +1,35 @@
-import React from 'react';
-import { View } from 'react-native';
-// import { Container } from './styles';
+import { useNavigation } from '@react-navigation/native';
+import React, { useCallback } from 'react';
+import Icon from 'react-native-vector-icons/Feather';
+import {
+  Container,
+  Title,
+  Description,
+  OKButton,
+  OKButtonText,
+} from './styles';
 
 const AppointmentCreated: React.FC = () => {
-  return <View style={{ flex: 1, justifyContent: 'center' }} />;
+  const { reset } = useNavigation();
+  const handleOkPressed = useCallback(() => {
+    reset({
+      routes: [{ name: 'Dashboard' }],
+      index: 0,
+    });
+  }, [reset]);
+  return (
+    <Container>
+      <Icon name="check" size={80} color="#04d361" />
+      <Title>Agendamento concluído</Title>
+      <Description>
+        Terça, dia 21 de Fevereiro de 2020 às 20:00 com Cláudio Assis.
+      </Description>
+
+      <OKButton onPress={handleOkPressed}>
+        <OKButtonText>Ok</OKButtonText>
+      </OKButton>
+    </Container>
+  );
 };
 
 export default AppointmentCreated;
